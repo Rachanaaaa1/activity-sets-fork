fdb39d7c5a7842d7100e1abcd777b361fe02cd4c
Nawaz Banu <121148792+nawazbanu@users.noreply.github.com>
2022-12-21 09:34:27 +0000

    prob1&2

--- set01/problem01.c
+++ set01/problem01.c
@@ -1,5 +1,12 @@
+//Write a C program to print your name.
+
 #include<stdio.h>
 
-int main(){
-  printf("Hello World");
+int main()
+{
+  printf("nawaz");
 }
+
+// cd set_
+// gcc problem_.c
+// ./a.out
---
+++ set01/problem02.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+  int a,b,sum;
+  a=3;
+  b=10;
+  sum=a+b;
+  printf("%d",sum);
+  //printf("Enter a number:");
+  //scanf("%d %d",&a,&b);
+}
