24095f5701ffcab553fabba71c995cb7414c5e26
Nawaz Banu <121148792+nawazbanu@users.noreply.github.com>
2023-01-20 08:24:21 +0000

    problem10.c

--- set01/problem10.c
+++ set01/problem10.c
@@ -1,20 +1,26 @@
 //Write a C program to compare two strings, character by character.
 
 #include<stdio.h>
+#include<string.h>
 
 void input_two_strings(char *string1, char *string2);
 int stringcompare(char *string1, char *string2);
 void output(char *string1, char *string2, int result);
 
 int main(){
-  char string1[60];
-  char string2[60];
-  int result;
+  char string1[100],string2[100];
   
-  input_two_strings(&string1 , &string2);
-  result = stringcompare(&string1,&string2);
-  output(&string1,&string2, int result);
+  input_two_strings(string1 , string2);
+ 
+  if(stringcompare(string1,string2) == 1){
+  output(string1,string2,1);
   }
+  else if(stringcompare(string1,string2) == 0){
+  output(string1,string2,0);
+    }
+  return 0;
+  }
+
 
 void input_two_strings(char *string1, char *string2){
   printf("Enter the first string: ");
@@ -24,27 +30,32 @@ void input_two_strings(char *string1, char *string2){
 }
 
 int stringcompare(char *string1, char *string2){
-  int result;
-  for(int i = 0; i < 60; i++){
-    if(string1[i] > string2[i]){
-      result = +1;
-    }
-      else if(string1[i] < string2[i]){
-        result = -1;
+  int a = strlen(string1);
+  int b = strlen(string2);
+  
+  
+    
+    int i;
+    
+    for(i=0; string1[i] != '\0' && string2[i] != '\0';i++){
+      if(string1[i] != string2[i])
+        return(0);
       }
-    else{
-      result = 0;
-    }
-  }
-  return result;
-  }
-
-
-void output(char *string1, char *string2, int result){
-if(result == 0){
+      if( string1[i] != '\0' && string2[i] != '\0'){
+      
+        return(1);
+        }
+      else{
+        return(0);
+      }  
+}
+  
+ void output(char *string1, char *string2, int result){
+if(result == 1){
   printf("same");
-}
-  else if(result == -1){
-  printf("%s is greater than %s",string2,string1);
-}
   }
+  else if(result == 0){
+  printf("%s is greater than %s",string2,string1);
+    }
+}
+  
