b6164d43260dfa27694165022b8cacbd30477d10
Nawaz Banu <121148792+nawazbanu@users.noreply.github.com>
2023-01-20 05:36:54 +0000

    problem12.c

--- set01/problem12.c
+++ set01/problem12.c
@@ -16,8 +16,8 @@ void output(int n, Complex c[n], Complex result);
 
 int main(){
   int n;
-  Complex c[n] , result;
   n = get_n();
+  Complex c[n] , result;
   input_n_complex(n ,c );
   result = add_n_complex(n ,c);
   output(n ,c,result);
@@ -61,7 +61,6 @@ Complex add_n_complex(int n, Complex c[n]){
   Complex sum = {0,0};
   for(i = 0 ; i < n ; i++){
     sum = add(c[i],sum);
-    c[i] = sum;
   }
   return sum;
 }
