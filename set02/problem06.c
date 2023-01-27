d86c427fb2deea5311ed2c7d10ae9a81f0035cce
Nawaz Banu <121148792+nawazbanu@users.noreply.github.com>
2023-01-27 06:42:56 +0000

    problem07.c

--- set02/problem06.c
+++ set02/problem06.c
@@ -3,11 +3,29 @@
 // output : olleh
 
 #include<stdio.h>
+#include<string.h>
 
 void input_string(char *a);
 void str_reverse(char *str, char *rev_str);
 void output(char *a, char *reverse_a);
 
 void input_string(char *a){
-  
+  char a;
+  printf("Enter the string \n");
+  scanf("%s",&a);  
+}
+
+void str_reverse(char *str, char *rev_str){
+  gets(str);
+  strrev(str);
+}
+
+void output(char *a, char *reverse_a){
+  printf("%s",strrev);
+}
+
+int main(){
+  input_string();
+  str_reverse();
+  output();
 }
--- set02/problem07.c
+++ set02/problem07.c
@@ -18,21 +18,21 @@ Triangle input_triangle(){
   Triangle t;
   printf("Enter base and altitude");
   scanf("%f %f",&t.base,&t.altitude);
-  }
+  return t;
+  }  
 
 void find_area(Triangle *t){
-
-  
   float area;
+  t -> area = (0.5) * (t -> base)*(t -> altitude);
 }
 
 void output(Triangle t){
-  printf("The area of triangle with base = %f and altitude = %f is %f");
+  printf("The area of triangle with base = %f and altitude = %f is %f",t.base,t.altitude,t.area);
 }
 
 int main(){
-  float base,altitude,area,t;
-  input();
+  Triangle t;
+  t = input_triangle();
   find_area(&t);
   output(t);
   return 0;
