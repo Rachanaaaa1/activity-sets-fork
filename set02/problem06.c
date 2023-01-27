5c0fdbd4dc06853aca726080b5e24f8bec8fd243
Nawaz Banu <121148792+nawazbanu@users.noreply.github.com>
2023-01-27 09:24:14 +0000

    problem06.c

--- set02/problem06.c
+++ set02/problem06.c
@@ -10,22 +10,31 @@ void str_reverse(char *str, char *rev_str);
 void output(char *a, char *reverse_a);
 
 void input_string(char *a){
-  char a;
   printf("Enter the string \n");
-  scanf("%s",&a);  
+  scanf("%s",a);  
 }
 
 void str_reverse(char *str, char *rev_str){
-  gets(str);
-  strrev(str);
-}
+int begin,end,count = 0;
+  while(str[count] != '\0')
+  {
+    count++;
+    end = count - 1;
+  }
+  for(begin = 0;begin < count;begin++){
+    rev_str[end] = str[begin];
+    end--;
+  }
+  rev_str[begin] = '\0';
+  }
 
 void output(char *a, char *reverse_a){
-  printf("%s",strrev);
+  printf("%s",reverse_a);
 }
 
 int main(){
-  input_string();
-  str_reverse();
-  output();
+  char str[50],rev_str[50];
+  input_string(str);
+  str_reverse(str,rev_str);
+  output(str,rev_str);
 }
--- set02/problem08.c
+++ set02/problem08.c
@@ -15,11 +15,12 @@ Triangle find_smallest_triangle(int n, Triangle t[n]);
 void output(int n, Triangle t[n], Triangle smallest);
 
 Triangle input_triangle(){
-  Triangle n;
+  Triangle t;
   printf("Enter the base");
-  scanf("%f",&n.base);
+  scanf("%f",&t.base);
   printf("Enter the height");
-  scanf("%f",&n.height);
+  scanf("%f",&t.height);
+  return t;
 }
 
 void input_n_triangles(int n, Triangle t[n]){
@@ -32,9 +33,40 @@ void input_n_triangles(int n, Triangle t[n]){
   
 void find_area(Triangle *t){
   float area;
-  
-
-
-  
+  t -> area = (0.5) * (t -> base) * (t -> altitude);   
 }
 
+void find_n_areas(int n, Triangle t[n]){
+  Triangle n,i,t[n];
+  for(i = 0;i < n;i++){
+  printf("Enter the areas \n");
+  scanf("%f",&t.area[i]);
+    }
+}
+
+Triangle find_smallest_triangle(int n, Triangle t[n]){
+  Triangle smallest;
+  smallest = t.area[0];
+  for(i = 0;i < n;i++){
+    if(t.area[i] < smallest){
+      smallest = t.area[i];
+    }
+  }
+  return smalllest;
+}
+
+void output(int n, Triangle t[n], Triangle smallest){
+  printf("The area of n triangles are %f",t.area[i]);
+  printf("The smallest triangle of all is %f",smallest);
+}
+
+int main(){
+  Triangle n,issmallest;
+t = Triangle input_triangle();
+input_n_triangles(n ,t);
+find_area(t);
+find_n_areas(n,t);
+issmallest = find_smallest_triangle(n,t);
+output(n , t ,smallest);
+
+}
