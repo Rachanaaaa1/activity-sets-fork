3386fa1eb2344ce8b0ad947a419b8070bd4e56a6
Nawaz Banu <121148792+nawazbanu@users.noreply.github.com>
2023-02-09 08:58:14 +0000

    problem04.c

--- set04/problem05.c
+++ set04/problem05.c
@@ -1,4 +1,4 @@
-//e a program to find the index of the largest number in an array
+//write a program to find the index of the largest number in an array
 // input 4 2 9 1 7    output : The index of the largest number in the array is 2
 
 #include<stdio.h>
--- set04/problem06.c
+++ set04/problem06.c
@@ -4,14 +4,20 @@
 
 #include<stdio.h>
 #include<string.h>
+#include<stdlib.h>
+
 
 void input_string(char *a);
 int count_words(char *string);
 void output(char *string, int no_words);
 
 int main(){
-
-  
+  char a;
+  int count,no_words;
+  input_string(&a);
+  count = count_words(&string);
+  output(string,no_words);
+  return 0;
 }
 
 void input_string(char *a){
@@ -21,5 +27,15 @@ void input_string(char *a){
 }
 
 int count_words(char *string){
-  
+  int count = 0;
+  char* token = strtok(string," ");
+  while(token != NULL){
+    count++;
+    token = strtok(NULL," ");
+  }
+  return count;  
+}
+
+void output(char *string, int no_words){
+  printf("The number of words in  %s is %d",string,no_words);
 }
