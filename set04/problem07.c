a3d558d6619de92c2c5d247d18184e0f4bb139a0
Nawaz Banu <121148792+nawazbanu@users.noreply.github.com>
2023-02-17 15:40:20 +0000

    problem07.c

--- set04/problem07.c
+++ set04/problem07.c
@@ -2,7 +2,6 @@
 //input : 9 6 and 5 6  output : 9/6 + 5/6 = 7/3
 
 #include<stdio.h>
-#include<math.h>
 
 typedef struct {
     int num, den;
@@ -11,30 +10,27 @@ typedef struct {
 Fraction input_fraction();
 int find_gcd(int a, int b);
 Fraction add_fractions(Fraction f1, Fraction f2);
-void output(Fraction f1, Fraction f2, Fraction f3, Fraction sum)
+void output(Fraction f1, Fraction f2, Fraction sum);
 
 int main(){
-  int a = input_fraction();
-  int b = input_fraction();
-  int gcd = find_gcd(a , b);
+  Fraction f1,f2,sum;
+  f1 = input_fraction();
+  f2 = input_fraction();
   sum = add_fractions(f1,f2);
   output(f1,f2,sum);
   return 0;
 }
 
 Fraction input_fraction(){
-  Fraction x;
-  printf("Enter the num1 and den1");
-  scanf("%d %d \n",&num1,&den1);
-  printf("Enter the num2 and den2");
-  scanf("%d %d \n",&num2,&den2);
-  return x;
+  Fraction f;
+  printf("Enter the num and den \n");
+  scanf("%d %d",&f.num,&f.den);
+  return f;
 }
 
 int find_gcd(int a, int b){
-  a = (num1 * den2) + (den1 * num2);
-  b = den1 * den2;
-  for(i = 1; i <= a && i <= b){
+  int i,gcd;
+  for(i = 1; i <= a || i <= b;i++){
     if(a % i == 0 && b % i == 0){
       gcd = i;
     }
@@ -43,11 +39,15 @@ int find_gcd(int a, int b){
 }
 
 Fraction add_fractions(Fraction f1, Fraction f2){
-  Fraction f3;
-  sum = f1 + f2;
+  Fraction sum;
+  sum.num = f1.num * f2.den + f2.num * f1.den;
+  sum.den = f1.den * f2.den;
+  int gcd = find_gcd(sum.num,sum.den); 
+  sum.num /= gcd;
+  sum.den /= gcd;
   return sum;
 }
 
-void output(Fraction f1, Fraction f2, Fraction f3, Fraction sum){
-  printf("(%d / %d) + (%d / %d) = %d / %d",f1,f2,f3,sum);
+void output(Fraction f1, Fraction f2, Fraction sum){
+  printf("(%d / %d) + (%d / %d) = %d / %d",f1.num,f1.den,f2.num,f2.den,sum.num,sum.den);
 }
